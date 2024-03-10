a38e634b028c126899ea97c3855a30c4c84d7ff7
zhiz <zhiz2023520@outlook.com>
2024-03-11 01:17:00 +0800

    添加子应用

--- meiduo_mall/meiduo_mall/settings.py
+++ meiduo_mall/meiduo_mall/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+1n^=cz*e4c7qgwo+h_@pg4=3yqrxn)nu05!a8!($7i+sw@i7w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.meiduo.site', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.users',
 ]
 
 MIDDLEWARE = [
--- meiduo_mall/meiduo_mall/urls.py
+++ meiduo_mall/meiduo_mall/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 
+def index(reauest):
+    return HttpResponse('ok')
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', index),
 ]
